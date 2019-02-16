8cf87812f81d8edbfab55730d94476654e232507
Matthew Philps <philps.matthew@gmail.com>
2019-02-16 16:27:58 +0900

    passed failing test

--- src/App.jsx
+++ src/App.jsx
@@ -3,15 +3,25 @@ import "./App.css";
 import Modal from "./Modal"
  
 class App extends React.Component{
+    constructor(props){
+        super(props)
+        this.state = {
+            modalButton: false
+        };
+    }
+
+    toggleModal(){
+        this.setState({modalButton: !this.state.modalButton})
+    }
+
     render(){
         return (
             <div>
-                <button>
+                <button onClick={this.toggleModal.bind(this)}>
                     Click To Render Modal
                 </button>
-                <Modal/>
+                {this.state.modalButton ? <Modal/> : null}
             </div>
-            
         )
     }
 }
