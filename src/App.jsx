1ec3594ab4e798499c293f6bb946028578f08e23
Matthew Philps <philps.matthew@gmail.com>
2019-02-16 17:00:03 +0900

    CSS for modal added

--- src/App.jsx
+++ src/App.jsx
@@ -20,6 +20,26 @@ class App extends React.Component{
                 <button onClick={this.toggleModal.bind(this)}>
                     Click To Render Modal
                 </button>
+                <div>
+                    <p>
+                    THIS IS THE MAIN BODY HERER HERE HERE HERE HERE HERE
+                    </p>
+                    <p>
+                    THIS IS THE MAIN BODY HERER HERE HERE HERE HERE HERE
+                    </p>
+                    <p>
+                    THIS IS THE MAIN BODY HERER HERE HERE HERE HERE HERE
+                    </p>
+                    <p>
+                    THIS IS THE MAIN BODY HERER HERE HERE HERE HERE HERE
+                    </p>
+                    <p>
+                    THIS IS THE MAIN BODY HERER HERE HERE HERE HERE HERE
+                    </p>
+                    <p>
+                    THIS IS THE MAIN BODY HERER HERE HERE HERE HERE HERE
+                    </p>
+                </div>
                 {this.state.showModal ? <Modal/> : null}
             </div>
         )
